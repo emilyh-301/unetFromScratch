66749e3893fb282b1005ecf89797f0c82328d61a
emilyh-301 <36495239+emilyh-301@users.noreply.github.com>
2021-10-20 13:42:26 -0400

    Initial commit

---
+++ train.py
@@ -0,0 +1,82 @@
+import torch
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from tqdm import tqdm
+import torch.nn as nn
+import torch.optim as optim
+from model import UNET
+#from utils import ( load_checkpoint, save_checkpoint, get_loaders, check_accuracy, save_predictions_as_imgs )
+
+# Hyperparams
+LEARNING_RATE = 1e-4
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+BATCH_SIZE = 16
+NUM_EPOCHS = 3
+NUM_WORKERS = 2
+IMAGE_HEIGHT = 160
+IMAGE_WIDTH = 240
+PIN_MEMORY = True
+LOAD_MODEL = False
+TRAIN_IMG_DIR = "data/train_images"
+TRAIN_MASK_DIR = "data/train_masks"
+VAL_IMG_DIR = "data/val_images"
+VAL_MASK_DIR = "data/val_masks"
+
+def train_fn(loader, model, optimizer, loss_fn, scaler):
+    loop = tqdm(loader)
+
+    for batch_idx, (data, targets) in enumerate(loop):
+        data = data.to(device = DEVICE)
+        targets = targets.float().unsqeeze(1).to(device = DEVICE)
+
+        #forward
+        with torch.cuda.amp.autocast():
+            predictions = model(data)
+            loss = loss_fn(predictions, targets)
+
+        #backward
+        optimizer.zero_grad()
+        scaler.scale(loss).backwards()
+        scaler.step(optimizer)
+        scaler.update()
+
+        #update tqdm loop
+        loop.set_postfix(loss=loss.item())
+
+
+
+def main():
+    train_transform = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_values=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+    val_transforms = A.Compose(
+        [
+            A.Resize(height=IMAGE_HEIGHT, width=IMAGE_WIDTH),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_values=255.0,
+            ),
+            ToTensorV2(),
+        ]
+    )
+
+    model = UNET(in_channels=3, out_channels=1).to(DEVICE)
+    loss_fn = nn.BCEWithLogitsLoss()
+
+
+if __name__ == '__main__':
+    main()
